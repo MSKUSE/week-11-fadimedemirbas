285e4dd941f6fedb00fde5bfc84932dd07e7d581
Fadime Demirbaş <161874983+fadimedemirbas@users.noreply.github.com>
2025-05-22 12:52:39 +0300

    Add files via upload

---
+++ week-11-fadimedemirbas/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new StackArray();
+        try {
+            stack.pop();
+        }catch (Exception e){
+
+            System.out.println();}
+
+        stack.pop();
+
+        Stack stack = new StackLinkedList();
+        stack.push("This is a string");
+        stack.push(5);
+        stack.peek();
+        System.out.println("Is empty "
+        +stack.isEmpty() );
+        System.out.println("Pop = " +
+                stack.pop());
+        stack.peek();
+    }
+}
